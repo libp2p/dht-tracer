6cb4b255522e696cc05b7b6854211937680fb3d1
Melanie Plaza <melanie@melanies-mbp.lan>
2019-07-10 16:24:04 -0700

    some more data transforming, try with eventlogssample2

--- src/App.js
+++ src/App.js
@@ -4,32 +4,118 @@ import './App.css'
 
 let fileReader
 
-const handleFileRead = (e) => {
-  // TODO: reject random files
-  const content = fileReader.result
-  const array = content.split('\n')
-  const formattedArray = array.map((event) => {
-    console.log(event)
-    try {
-      return JSON.parse(event)
-    } catch {
-      return { error: 'incorrectly formatted event' }
-    }
-  })
-  console.log('array', formattedArray)
-  // … do something with the 'content' …
-}
-
-const handleFileChosen = (file) => {
-  fileReader = new FileReader()
-  fileReader.onloadend = handleFileRead
-  fileReader.readAsText(file)
-}
-
-const firstDate = new Date('2019-07-09T14:25:04.342982-07:00')
-
 class App extends Component {
+  state = {
+    data: null,
+  }
+  handleFileRead = (e) => {
+    // TODO: reject random files
+    const content = fileReader.result
+    const array = content.split('\n')
+    const formattedArray = array.map((event) => {
+      try {
+        return JSON.parse(event)
+      } catch {
+        return { error: 'incorrectly formatted event' }
+      }
+    })
+
+    function parseLogs(array) {
+      return array.map((event) => {
+        const logs = event.Logs.map((log) => {
+          return log.Fields[0]
+        })
+        for (let i = 0; i < logs.length; i++) {
+          const log = logs[i]
+          if (log.Key === 'PeersQueried' || log.Key === 'PeersSeen') {
+            const peerString = log.Value.slice(1, -1)
+            const peerArr = peerString.split(' ')
+            event[log.Key] = peerArr
+          } else {
+            event[log.Key] = log.Value
+          }
+        }
+
+        return event
+      })
+    }
+
+    const queryStart = parseLogs(
+      formattedArray.filter(
+        (event) => event.Operation === 'dhtQueryRunnerStart',
+      ),
+    )
+    const queryId = queryStart[0].query
+    console.log('query id is', queryId)
+    const queryArray = parseLogs(
+      formattedArray.filter((event) => event.Operation === 'queryPeer!'),
+    )
+    const dialArray = parseLogs(
+      formattedArray.filter((event) => event.Operation === 'dialPeer!'),
+    )
+    const queryRunnerArray = parseLogs(
+      formattedArray.filter((event) => event.Operation === 'dhtQueryRunner'),
+    )
+    const queryRunner = queryRunnerArray[0]
+    console.log('query runner is', queryRunner)
+    const data = [
+      [
+        { type: 'string', id: 'Peer' },
+        { type: 'string', id: 'Operation' },
+        { type: 'date', id: 'Start' },
+        { type: 'date', id: 'End' },
+      ],
+    ]
+    if (queryRunner) {
+      data.push([
+        'Query',
+        `Seen: ${queryRunner.PeersSeen.length}, Queried: ${
+          queryRunner.PeersQueried.length
+        }, ${(queryRunner.Duration / 1000000).toFixed(2)}s`,
+        new Date(queryRunner.StartTime),
+        new Date(queryRunner.EndTime),
+      ])
+    }
+    for (let i = 0; i < queryArray.length; i++) {
+      const peerQuery = queryArray[i]
+      const dataToGraph = [
+        `Peer ${peerQuery.peer}`,
+        'querying',
+        new Date(peerQuery.Start),
+        new Date(
+          new Date(peerQuery.Start).getTime() + peerQuery.Duration / 1000000,
+        ),
+      ]
+      data.push(dataToGraph)
+    }
+
+    for (let i = 0; i < dialArray.length; i++) {
+      const peerQuery = dialArray[i]
+      const dataToGraph = [
+        `Peer ${peerQuery.peer}`,
+        'dialing',
+        new Date(peerQuery.Start),
+        new Date(
+          new Date(peerQuery.Start).getTime() + peerQuery.Duration / 1000000,
+        ),
+      ]
+      data.push(dataToGraph)
+    }
+
+    this.setState({
+      data: data,
+    })
+  }
+
+  handleFileChosen = (file) => {
+    fileReader = new FileReader()
+    fileReader.onloadend = this.handleFileRead
+    fileReader.readAsText(file)
+  }
   render() {
+    const { data } = this.state
+    console.log('data is', data)
+
     return (
       <div>
         <div className="upload-expense">
@@ -37,72 +123,23 @@ class App extends Component {
             type="file"
             id="file"
             className="input-file"
-            onChange={(e) => handleFileChosen(e.target.files[0])}
+            onChange={(e) => this.handleFileChosen(e.target.files[0])}
           />
         </div>
         <div className={'my-pretty-chart-container'}>
-          <Chart
-            width={'800px'}
-            height={'300px'}
-            chartType="Timeline"
-            loader={<div>Loading Chart</div>}
-            data={[
-              [
-                { type: 'string', id: 'Operation' },
-                { type: 'date', id: 'Start' },
-                { type: 'date', id: 'End' },
-              ],
-              ['findPeerSingle', 0, 141324740 / 1000],
-              [
-                'findPeerSingle',
-                Math.abs(
-                  new Date('2019-07-09T14:25:04.343077-07:00') - firstDate,
-                ),
-                Math.abs(
-                  new Date('2019-07-09T14:25:04.343077-07:00') -
-                    firstDate +
-                    141772488 / 1000,
-                ),
-              ],
-              [
-                'findPeerSingle',
-                Math.abs(
-                  new Date('2019-07-09T14:25:04.34334-07:00') - firstDate,
-                ),
-                Math.abs(
-                  new Date('2019-07-09T14:25:04.34334-07:00') -
-                    firstDate +
-                    141538112 / 1000,
-                ),
-              ],
-              [
-                'dhtQueryRunner.Run',
-                Math.abs(
-                  new Date('2019-07-09T14:25:04.342463-07:00') - firstDate,
-                ),
-                Math.abs(
-                  new Date('2019-07-09T14:25:04.342463-07:00') -
-                    firstDate +
-                    142544168 / 1000,
-                ),
-              ],
-              [
-                'FindPeer',
-                Math.abs(
-                  new Date('2019-07-09T14:25:04.34239-07:00') - firstDate,
-                ),
-                Math.abs(
-                  new Date('2019-07-09T14:25:04.34239-07:00') -
-                    firstDate +
-                    142709256 / 1000,
-                ),
-              ],
-            ]}
-            options={{
-              showRowNumber: true,
-            }}
-            rootProps={{ 'data-testid': '1' }}
-          />
+          {data && (
+            <Chart
+              width={'100vw'}
+              height={'100vh'}
+              chartType="Timeline"
+              loader={<div>Loading Chart</div>}
+              data={data}
+              options={{
+                showRowNumber: true,
+              }}
+              rootProps={{ 'data-testid': '1' }}
+            />
+          )}
         </div>
       </div>
     )
